6caffa067d192fa021478dacde2c9de485f4e872
bhanum2 <noreply@gmail.com>
2018-06-13 12:37:06 +0000

    changing jupyter of anaconda

--- scripts/sample_notebook.sh
+++ scripts/sample_notebook.sh
@@ -5,5 +5,5 @@ sudo /usr/sbin/nginx
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/local/cuda/targets/ppc64le-linux/lib/
 export PYTHONPATH=$PYTHONPATH:/opt/anaconda2/lib/python2.7/site-packages:/opt/DL/tensorflow/lib/python2.7/site-packages
 export SPARK_HOME=/usr/local/spark-2.2.0-bin-hadoop2.7/
-jupyter notebook --no-browser --NotebookApp.token="" &
+/opt/anaconda2/bin/jupyter notebook --no-browser --NotebookApp.token="" &
 sudo /usr/sbin/sshd -D
